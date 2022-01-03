100db0b86ee38cf865ce7a547df09622379534c5
Illensky <alexis.laroche.02240@gmail.com>
2022-01-03 09:52:00 +0100

    first style steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ const cardsArray = [
     {
         name: 'jq',
         img: 'assets/img/jq.png'
-    },
+    },/*
     {
         name: 'node',
         img: 'assets/img/node.png'
@@ -78,20 +78,28 @@ const cardsArray = [
     {
         name: 'w',
         img: 'assets/img/w.png'
-    }
+    }*/
 ];
 
-cardsArray.sort(() => 0.5 - Math.random())
+
 
 const grid = document.querySelector(".grid");
-const resultDisplay = document.querySelector("#result")
+const resultDisplay = document.querySelector("#result");
+const startBtn = document.querySelector("#startBtn");
+const restartBtn = document.querySelector("#tryAgainBtn")
+const homeSection = document.querySelector("#home");
+const gameSection = document.querySelector("#game");
+const scoreSection = document.querySelector("#score");
+const info = document.querySelector("#info");
 let cardsChosen = [];
 let cardsChosenId = [];
 let cardsWon = [];
+let tryCounter = 0;
 
 //create the bord
 
 function createBoard(gameCardArray, gameBoard) {
+    gameCardArray.sort(() => 0.5 - Math.random())
     for (let i = 0; i < gameCardArray.length; i++) {
         let card = document.createElement('img');
         card.setAttribute('src', 'assets/img/blank.png');
@@ -109,18 +117,21 @@ function checkForMatch() {
         cards[optionOneId].setAttribute('src', 'assets/img/white.png');
         cards[optionTwoId].setAttribute('src', 'assets/img/white.png');
         cardsWon.push(cardsChosen);
-        alert('You found a match');
+        info.innerHTML = "Vous avez trouver une paire !! ";
     }
     else {
         cards[optionOneId].setAttribute('src', 'assets/img/blank.png');
         cards[optionTwoId].setAttribute('src', 'assets/img/blank.png');
-        alert('Try again !')
+        info.innerHTML = "Essayez encore !!"
     }
     cardsChosen = [];
     cardsChosenId = [];
+    tryCounter++;
     resultDisplay.innerHTML = cardsWon.length.toString()
     if (cardsWon.length === cardsArray.length/2) {
-        resultDisplay.innerHTML = "you win !!"
+        gameSection.style.display = "none";
+        scoreSection.style.display = "block";
+        resultDisplay.innerHTML = "Vous avez réussit en " + tryCounter.toString() + " essais pour " + (cardsArray.length /2).toString() + " paire.";
     }
 }
 
@@ -134,4 +145,19 @@ function flipCard() {
     }
 }
 
-createBoard(cardsArray, grid)
+startBtn.addEventListener("click", () => {
+    homeSection.style.display = "none";
+    gameSection.style.display = "block"
+    createBoard(cardsArray, grid);
+})
+
+restartBtn.addEventListener("click", () => {
+    scoreSection.style.display = "none";
+    cardsChosen = [];
+    cardsChosenId = [];
+    cardsWon = [];
+    grid.innerHTML = ""
+    tryCounter = 0;
+    createBoard(cardsArray, grid);
+    gameSection.style.display = "block";
+})
